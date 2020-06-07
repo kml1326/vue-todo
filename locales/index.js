e6ea01dee679f1329dfd7990140c15606ec32945
km1326 <komal.raj@khelomore.com>
2020-06-07 06:41:58 +0530

    added locales folder

---
+++ locales/index.js
@@ -0,0 +1,36 @@
+const messages = {
+  "en-US": {
+    hello: "Hello, {name}!",
+    addTodo: "Add ToDo",
+    enterMobileNumber: "Enter Mobile Number",
+    enterName: "Enter Name",
+    mobile: "Mobile Numbar",
+    login: "Login",
+    numberErrorMsg: "Please enter 10 digit number!",
+    nameErrorMsg: "Please enter name",
+    selectLanguage: "Select Language",
+    modeSelection: "Mode Selection",
+    dark: "Dark",
+    light: "Light",
+    delete: "Delete",
+    logout: "Logout"
+  },
+  "hn-IN": {
+    hello: "नमस्कार, {name}!",
+    addTodo: "ToDo जोड़ें",
+    enterMobileNumber: "मोबाइल नंबर दर्ज करें",
+    enterName: "नाम दर्ज करें",
+    mobile: "मोबाइल नंबर",
+    login: "लॉग इन करें",
+    numberErrorMsg: "कृपया 10 अंकों की संख्या दर्ज करें!",
+    nameErrorMsg: "कृपया नाम दर्ज करें!",
+    selectLanguage: "भाषा का चयन करें",
+    modeSelection: "मोड चयन करें",
+    dark: "अंधेरा",
+    light: "रोशनी",
+    delete: "हटाएं",
+    logout: "लॉग आउट"
+  }
+};
+
+export default messages;
