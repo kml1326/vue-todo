0d38146fd639205e75a6221d4c751863229301cb
km1326 <komal.raj@khelomore.com>
2020-06-07 06:39:45 +0530

    crated store, roue and updated App

--- src/store/index.js
+++ src/store/index.js
@@ -3,9 +3,40 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+export const store = new Vuex.Store({
+  state: {
+    todoList: []
+  },
+  mutations: {
+    setTodo(state, todo) {
+      state.todoList.push(todo);
+    },
+    toggleTodo(state, id) {
+      let array = [];
+      state.todoList.map((todo, idx) => {
+        if (idx === id) {
+          array.push({ text: todo.text, done: !todo.done });
+        } else {
+          array.push(todo);
+        }
+      });
+    },
+    deleteTodo(state, id) {
+      state.todoList.splice(id, 1);
+    }
+  },
+  getters: {
+    todoList: state => state.todoList
+  },
+  actions: {
+    setTodo({ commit }, todo) {
+      commit("setTodo", todo);
+    },
+    toggleTodo({ commit }, id) {
+      commit("toggleTodo", id);
+    },
+    deleteTodo({ commit }, id) {
+      commit("deleteTodo", id);
+    }
+  }
 });
