f4fb558f2aa1757440e79cc0a570afaec5965a50
km1326 <komal.raj@khelomore.com>
2020-06-07 06:40:41 +0530

    updated main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,22 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from "./store";
+import { store } from "./store";
+import VueI18n from "vue-i18n";
+import messages from "../locales";
+
+Vue.use(VueI18n);
+
+export const i18n = new VueI18n({
+  locale: "en-US",
+  messages
+});
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  i18n,
   render: h => h(App)
 }).$mount("#app");
